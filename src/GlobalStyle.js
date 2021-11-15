274f5bd8f45a1dbe41c55dbd6ea56b9cd07d416f
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-11-15 12:31:28 -0500

    GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,46 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@600;800&family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap');
+    
+    * {
+        font-family: "Open Sans", sans-serif;
+    }
+    
+    html {
+        box-sizing: border-box;
+        font-size: 62.5%;
+        line-height: 1;
+    }
+    
+    *, *:before, *:after {
+        box-sizing: inherit;
+    }
+    
+    body {
+        margin: 0;
+        padding: 0;
+        
+        h1 {
+            font-size: 3rem;
+            font-weight: 800;
+        }
+        
+        h2 {
+            font-size: 1.8rem;
+            font-weight: 600;
+        }
+        
+        p {
+            font-size: 1.2rem;
+            font-weight: 400;
+        }
+    }
+`
+
+// centering content! Better method!
+export const Container = styled.div`
+    height: auto;
+    max-width: 960px;
+    margin: auto;
+`
