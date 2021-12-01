f19696341029b52e5b3db104e58657d44dae68b8
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-12-01 11:09:42 -0500

    Updates

---
+++ precache-manifest.89efbffc588a72170c385d41d868bd27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9939e6ad6f10840fbae4e62dfa8c9329",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "ff8375f5b06ee20199d0",
+    "url": "/react-todo-list/static/js/2.8ea323c8.chunk.js"
+  },
+  {
+    "revision": "77eac2b92e272547944076641dfc58d6",
+    "url": "/react-todo-list/static/js/2.8ea323c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7894777875ddc823c0d6",
+    "url": "/react-todo-list/static/js/main.6daf8754.chunk.js"
+  },
+  {
+    "revision": "a4d4aadd95de1f9c2774",
+    "url": "/react-todo-list/static/js/runtime-main.cca871c0.js"
+  }
+]);
